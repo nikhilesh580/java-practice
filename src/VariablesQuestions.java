b64cea3fe63c60511b325d39ed3cd7203bfb2971
nikhilesh580 <nikhileshtripathi50@gmail.com>
2026-04-18 17:58:08 +0530

    Add VariablesQuestions.java -50 practice Questions

---
+++ src/VariablesQuestions.java
@@ -0,0 +1,104 @@
+public class VariablesQuestions {
+
+    public static void main (String[] args){
+
+        //LEVEL 1 VARIABLES QUESTION (1 TO 15)
+
+        //DAY ---01
+
+
+        int num = 10;
+        System.out.println("print num:" + num );
+
+        double num2 = 5.5;
+        System.out.println("print num2:" + num2);
+
+        char grade ='A';
+        System.out.println("print grade:" +grade);
+
+         String name = "Nikhildon";
+         System.out.println("my name is:" + name);
+
+         boolean isStudent = true;
+         System.out.println("mai hu yaa nhi:" + isStudent);
+
+         //make two variable then print
+
+        char myvar1 = 78;
+        char myvar2 = 82;
+
+        System.out.println("first variable:" + myvar1);
+        System.out.println("second variable:" + myvar2);
+
+        //print value of a and b
+
+        int a = 5;
+        int b = 10;
+
+        System.out.println("first variable a:" + a);
+        System.out.println("second variable b:" + b);
+
+        //print x+y
+        int x = 15;
+        int y= 59;
+
+        System.out.println("sum:" +(a+b));
+
+        //print p-q
+        int p = 12;
+        int q = 23;
+
+        System.out.println("minus:" + (p-q));
+
+        //print A*B
+        int A=89;
+        int B=6589;
+
+        System.out.println("multiply:" + (A*B));
+
+        //PRINT Y/Z
+
+        int Y = 98;
+        int Z = 2;
+
+        System.out.println("divide:" + (Y/Z));
+
+        // PUT DECIMAL IN INT
+
+          /*int decimal = 90.3; //int kewal whole numbers print krta hai
+
+        System.out.println(decimal);*/
+
+
+
+        // Add string + string
+
+        String firstname = "Nikhil";
+        String lastName= "tripathi";
+
+        String fullName = firstname+ " " + lastName;
+
+        System.out.println(fullName);
+
+        //int + string print krna hai
+
+        int sprite = 10;
+        String coldrink = "sprite";
+
+        System.out.println("int value: " + sprite);
+        System.out.println("string: " + coldrink);
+
+
+        //one variable change value
+
+        int mynum = 10;
+        System.out.println("BEFORE: "+ mynum);
+
+        mynum = 30;
+        System.out.println("AFTER: " + mynum);
+
+
+
+
+    }
+}
--- src/variablesQuestions.java
+++
@@ -1,7 +0,0 @@
-public class variablesQuestions {
-
-    public static void main (String[] args){
-        int num = 10;
-        System.out.println("print num:" +num );
-    }
-}
