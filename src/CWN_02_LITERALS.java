23e02a33d94cae4676e522e4bf725734e2f9ae03
nikhilesh580 <nikhileshtripathi50@gmail.com>
2026-04-27 00:10:22 +0530

    CWN_02_LITERALS.JAVA

--- src/CWN_02_LITERALS.java
+++ src/CWN_02_LITERALS.java
@@ -1,3 +1,18 @@
-public class CWN_02_LITERALS {
+import java.sql.SQLOutput;
 
+public class CWN_02_LITERALS {
+         public static void main (String[] args){
+             byte age =34;
+             int age2 = 56;
+             long agenick = 2065987126555122l;
+             short age3 = 98;
+             char ch = 'a';
+             float f1 = 4.54f;
+             double d1 = 5643.65;
+             boolean a = true;
+             System.out.println(age);
+             String str = "nikhil";
+             System.out.println(str);
+
+    }
 }
