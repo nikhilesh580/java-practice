6143623840e23fde22402f63e830e3ae272b9580
nikhilesh580 <nikhileshtripathi50@gmail.com>
2026-04-28 21:29:57 +0530

    Practiceset1.java

---
+++ src/Practiceset1.java
@@ -0,0 +1,161 @@
+import java.util.Scanner;
+public class Practiceset1 {
+    public static void main(String[] args){
+
+        //Q.1 Percentage of student in 5 subject
+
+        Scanner sc = new Scanner(System.in);
+//        int m1 = sc.nextInt();
+//        int m2 = sc.nextInt();
+//        int m3 = sc.nextInt();
+//        int m4 = sc.nextInt();
+//        int m5 = sc.nextInt();
+//
+//        double sum  = m1+m2+m3+m4+m5;
+//        double percentage = sum/5.0;
+//
+//
+//        System.out.println(percentage);
+
+
+        // Q.2 sum of numbers
+//        System.out.println("Enter number 1");
+//        int a = sc.nextInt();
+//        System.out.println("Enter number 2");
+//        int b = sc.nextInt();
+//        System.out.println("Enter number 3");
+//        int c = sc.nextInt();
+//
+//        int sum = a+b+c;
+//        System.out.println("the sum of these number is ");
+//        System.out.println(sum);
+
+
+        //Q.3 CGPA calculator
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        int c = sc.nextInt();
+//
+//        double sumofmarks = a+b+c;
+//        double CGPA = sumofmarks/300*10;
+//        System.out.println(CGPA);
+
+        //Q.3 Hello<name>greeter
+
+//        System.out.println("Enter your name: ");
+//        String name = sc.nextLine();
+//        System.out.println("Hello " + name + " , Have a good day");
+
+
+        //km to miles
+//        System.out.println("Enter kilometers: ");
+//        double km = sc.nextDouble();
+//        double miles = km*0.621371;
+//
+//        System.out.println(km + "km = " + miles + "miles");
+
+
+
+        //integet detect karna
+//        System.out.println("Enter a number: ");
+//        String input = sc.nextLine();
+//        try{
+//            Integer.parseInt(input);
+//            System.out.println(input + " is an integer");
+//        } catch (Exception e){
+//            System.out.println(input + " is not an integer");
+//        }
+
+
+        //  check even and odd
+//        int num = sc.nextInt();
+//
+//        if(num % 2 == 0) {
+//            System.out.println("Even");
+//        } else {
+//            System.out.println("odd");
+//        }
+
+
+        //user se name lo and print karo
+
+//        System.out.println("Enter your name: ");
+//        String str= sc.nextLine();
+//        System.out.println("my name is:" + str);
+
+        // consider three number and check largest
+
+//        System.out.println("Enter number 1");
+//        int num1 = sc.nextInt();
+//        System.out.println("Enter number 2");
+//        int num2 = sc.nextInt();
+//        System.out.println("Enter number 3");
+//        int num3 = sc.nextInt();
+//
+//        if(num1>num2 && num1>num3){
+//            System.out.println("num1 is the largest");
+//        } else if(num2>num1 && num2>num3){
+//            System.out.println("num2 is the largest");
+//        } else {
+//            System.out.println("num3 is the largest");
+//
+//        }
+
+        //user se input lo grade print karo
+
+//        System.out.println("Enter marks: ");
+//        int marks = sc.nextInt();
+//        if(marks >= 90){
+//            System.out.println("Grade A");
+//        }
+//        else if (marks >= 75){
+//            System.out.println("Grade B");
+//        } else if (marks >= 50){
+//            System.out.println("Grade C");
+//        }
+//        else {
+//            System.out.println("Fail");
+//        }
+
+        //consider nextint() + nextDouble()
+//        System.out.println("Enter int value");
+//        int num1 = sc.nextInt();
+//        System.out.println("Enter double value");
+//        double num2 = sc.nextDouble();
+//        System.out.println("Integer =" + num1);
+//System.out.println("Double =" + num2);
+
+
+        //consider next() vs nextline()
+
+//        System.out.println("Enter next() sentence:");
+//        String str1 = sc.next();
+//        sc.nextLine();
+//        System.out.println("Enter nextline() sentence");
+//        String str2 = sc.nextLine();
+//
+//        System.out.println("first sentence: " + str1);
+//        System.out.println("Second sentence: " + str2);
+
+
+        //Boolean value
+//        System.out.println("Enter boolean value");
+//        Boolean istudent = sc.nextBoolean();
+//        System.out.println( "value is:"+ istudent);
+
+        //MIXED INPUT
+
+//        System.out.println("Print my name is:");
+//        String str= sc.nextLine();
+//        System.out.println("Print my age:");
+//        int age = sc.nextInt();
+//        System.out.println("Print my marks:");
+//        int marks = sc.nextInt();
+//
+//        System.out.println("Name:"+ str);
+//        System.out.println("age:" + age);
+//        System.out.println("Marks:" + marks);
+
+    }
+}
+
