6032bb6b60ec2a9229059cc4b0d7213d04eb2d1d
nikhilesh580 <nikhileshtripathi50@gmail.com>
2026-05-02 01:40:19 +0530

    CWN_09_PS_PR3.java

---
+++ src/CWN_09_PS_PR3.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class CWN_09_PS_PR3 {
+    public static void main (String[]args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a>23);
+
+    }
+}
