3f44c8a1a82aeaa7106ffb31612a4066ca7365b1
nikhilesh580 <nikhileshtripathi50@gmail.com>
2026-05-01 23:58:28 +0530

    CWN_04_operators.java

---
+++ src/CWN_04_operators.java
@@ -0,0 +1,26 @@
+import java.sql.SQLOutput;
+
+public class CWN_04_operators {
+    public static void main (String[]args){
+        // Arithmetic Operators
+//        int a=4;
+//        int b = 6%a;  //MODULO OPERATOR
+//        System.out.println(b);
+
+        // ASSIGNMENT OPERATORS
+//        int a = 23;
+//         int b = 3;
+//         b *= 34;
+//        System.out.println(b);
+
+        // COMPARISON OPERATORS
+//        System.out.println(67>2);
+
+        // LOGICAL OPERATORS
+//        System.out.println(123>12 && 9>2); //  dono value true honi chahiye otherwise false print honga
+//        System.out.println(98>13 || 45>46); // kisi ak side ki bhi value true hai to true print honga dusri side skip ho jayegi
+
+        //BITWISE OPERATORS
+//        System.out.println(2&3);
+    }
+}
