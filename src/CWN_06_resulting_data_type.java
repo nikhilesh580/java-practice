9f07882ae16f0427a1e8453bea1484acde3a3cc4
nikhilesh580 <nikhileshtripathi50@gmail.com>
2026-05-02 01:06:14 +0530

    CWN_06_resulting_data_type.java

---
+++ src/CWN_06_resulting_data_type.java
@@ -0,0 +1,68 @@
+public class CWN_06_resulting_data_type {
+    public static void main (String []args){
+    /*
+        Byte x=5;
+        int y = 3;
+        short z = 8;
+        int a = y+z;
+        float b = 6.54f + z;
+        System.out.println(b);*/
+
+
+        //INCREMENT AND DECREMENT OPERATORS
+
+        //   POST INCREMENT = a++.
+        //   PRE INCREMENT = a--.
+
+        //POST DECREMENT = a--.
+        //PRE DECREMENT = --a.
+
+
+
+//        int i = 34;
+//        System.out.println(i++ + " " + i); // output: 34 35
+//        System.out.println(++i + " " + i);// output: 53 35
+
+//        int j =  23;
+//        System.out.println(j-- + " " + j);
+//        System.out.println(--j + " " + j);
+
+
+        //QUICK QUIZ: (USE INCREMENT AND DECREMENT OPERATOR IN VARIABLE)
+//        System.out.println("Enter first:");
+//         int a = 23;
+//         int b = a++;
+//        System.out.println(b);
+//        System.out.println(a);
+
+//        System.out.println("Enter second:");
+//        int x = 3;
+//        int y = ++a;
+//        System.out.println(x);
+//        System.out.println(y);
+//
+//        System.out.println("Enter third:");
+//        int p = 5;
+//        int q = a--;
+//        System.out.println(p);
+//        System.out.println(q);
+//
+//        System.out.println("Enter fourth:");
+//        int r = 6;
+//        int n = --a;
+//        System.out.println(r);
+//        System.out.println(n);
+
+
+        //QUIZ SECOND
+
+//        int y = 7;
+//        int x = ++y * 8 ;
+//        System.out.println(x);
+//        System.out.println(y);
+
+//        char ch = 'b';
+//        System.out.println(++ch);
+//        System.out.println(--ch);
+    }
+}
