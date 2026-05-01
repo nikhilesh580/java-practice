3b0c25cc77eece8e826ba22ea6acb3622f4b7344
nikhilesh580 <nikhileshtripathi50@gmail.com>
2026-05-01 12:27:39 +0530

    Practiceset1.java

---
+++ src/practiceset_02.java
@@ -0,0 +1,179 @@
+import java.util.Scanner;
+public class practiceset_02 {
+    public static void main(String []args){
+        Scanner sc = new Scanner (System.in);
+//        System.out.println("Enter first number is:");
+//        int num1  = sc.nextInt();
+//        System.out.println("Enter second number is ");
+//        int num2 = sc.nextInt();
+//        System.out.println("Enter third number is ");
+//        int num3 = sc.nextInt();
+//
+//        int sum = num1+num2+num3;
+//        System.out.println("SUM=" +sum);
+//
+//        double avg = sum/3.0;
+//        System.out.println("Average =" + avg);
+
+
+
+
+        //ASCII VALUE FINDER
+
+//        System.out.println("Enter a character:");
+//        char ch = sc.next().charAt(0);
+//
+//        int ascii = ch;
+//        System.out.println("ASCII value=" + ascii);
+
+        //Swap without third variable
+
+//        System.out.println("Enter first number:");
+//        int a = sc.nextInt();
+//
+//        System.out.println("Enter secon number");
+//        int b = sc.nextInt();
+//
+//        //logic
+//        a=a+b;
+//        b=a-b;
+//        a=a-b;
+//
+//        System.out.println("After swapping:");
+//        System.out.println("a=" +a);
+//        System.out.println("b=" +b);
+
+
+        //Area of circle
+//        System.out.println("Enter area of circle:");
+//        double radius = sc.nextDouble();
+//        double pi = 3.14;
+//
+//        double area = pi*radius*radius;
+//        System.out.println("AREA:" + area);
+
+
+        // EXPRESSION EVALUATION
+
+//        System.out.println("Enter value of  a:");
+//        int a = sc.nextInt();
+//        System.out.println("Enter value of b:");
+//        int b = sc.nextInt();
+//        System.out.println("Enter value of c:");
+//        int c = sc.nextInt();
+//        System.out.println("a*b*c= " + (a+b*c));
+//        System.out.println("(a+b)*c =" +((a+b)*c));
+//        System.out.println("a*b/c=" +(a*b/c));
+
+
+
+        //EVEN  ODD WITHOUT %
+
+//        System.out.println("Enter a number:");
+//        int n = sc.nextInt();
+//
+//        if ((n /2) *2 ==n) {
+//            System.out.println("Even");
+//        } else {
+//            System.out.println("Odd");
+//        }
+
+
+        //hard logical questions
+
+        //Q.1 increment logic
+
+//        System.out.println("Enter these value: ");
+//        int a = sc.nextInt();
+//        int result = a + a++ + ++a + a;
+//        System.out.println("Result:" + result);
+//        System.out.println("Enter final value of a:" + a);
+
+
+        //Q.2 another increment que
+
+//        System.out.println("Enter increment number is:");
+//        int a = sc.nextInt();
+//        int result = a++ + ++a + a++ + ++a + a++ + a;
+//        System.out.println("Result:" + result);
+//        System.out.println("Final value of a:" + a);
+
+
+        //Decrement logic
+
+//        System.out.println("Enter these value:");
+//        int a = sc.nextInt();
+//        int result = a-- + --a + a-- + --a +a;
+//        System.out.println("RESULT:" + result);
+//        System.out.println("final value of a:" +a);
+
+
+        // boolean+increment+decrement
+
+
+//        System.out.println("Print value of a ");
+//        int  a = sc.nextInt();
+//        System.out.println("Print value of b");
+//        int b= sc.nextInt();
+//        boolean result = a++> 5 && --b>9;
+//        System.out.println("Result:" + result) ;
+
+
+        //increment and decrement
+//        System.out.println("Enter these value");
+//        System.out.println("Print first value:");
+//        int x = sc.nextInt();
+//        System.out.println("Print second value");
+//        int y = sc.nextInt();
+//        boolean result = (++x > 3) || (y-- > 6);
+//        System.out.println("Result:" + result);
+
+        //ternary opreator
+//        System.out.println("Enter first value:");
+//        int val1 = sc.nextInt();
+//        System.out.println("Enter second value:");
+//        int val2 = sc.nextInt();                  // sirf use ho raha hai
+//        int result = (val1++ > 4) ? val2 : val1;
+//        System.out.println( "Result:" +result);
+//        System.out.println("Final val1:" + val1);
+
+        // boolean+short circuit
+
+//        System.out.println("Enter these number:");
+//        System.out.println("Enter first value:");
+//        int a  = sc.nextInt();
+//        System.out.println("Enter second value:");
+//        int b = sc.nextInt();
+//        boolean result = a++ > 2 && --b > 4 || a++ < 6;
+//        System.out.println("Result:" + result);
+//        System.out.println("Final value of a:" + a);
+//        System.out.println("Final value of b:" + b);
+
+        //ternary+side effect
+//        System.out.println("enter these value");
+//        System.out.println("Enter first value:");
+//        int a = sc.nextInt();
+//        System.out.println("enter second value:");
+//        int b = sc.nextInt();
+//        int result = (a++ > 5) ? b++ : ++a+b;
+//        System.out.println("Result:" + result);
+//        System.out.println("Final value of a:" + a);
+//        System.out.println("Final value of b:"+ b);
+
+
+        //ultimate combo
+
+//        System.out.println("enter these value:");
+//        System.out.println("Enter first value:");
+//        int a =sc.nextInt();
+//        System.out.println("Enter second value:");
+//        int b = sc.nextInt();
+//        int result = a++ > 3  || --b >5 && ++a > 4 ? a++ + b-- : --a + ++b;
+//
+//        System.out.println("RESULT:" + result);
+//        System.out.println("final value of a:" + a);
+//        System.out.println("Final value of b:" + b);
+
+    }
+
+}
