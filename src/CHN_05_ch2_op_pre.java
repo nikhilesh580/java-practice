b2e793662436c42d80aeee3f4d27a3fa2840a7b8
nikhilesh580 <nikhileshtripathi50@gmail.com>
2026-05-02 00:21:47 +0530

    CWN_05_ch2_op_pre.java

---
+++ src/CHN_05_ch2_op_pre.java
@@ -0,0 +1,39 @@
+public class CHN_05_ch2_op_pre {
+    public static void main(String []args) {
+        //Precedence & associativity
+
+//        int a = 6*5-658/2;
+
+        /*
+  //      Highest precedence goes to * and /.
+    //    They are then evaluated on the basis of left to right associativity.
+
+        =30-658/2
+        =30-329
+        =-299
+         */
+
+//        int b = 60/5-34*2;
+        /*
+           =12-34*2
+           =12-68
+           =-56
+         */
+//        System.out.println(a);
+//        System.out.println(b);
+
+        // Quick Quiz
+
+//        int x = 6;
+//        int y= 1;
+//        int k = x*y/2;
+//        System.out.println(k);
+//
+//        int a= 2;
+//        int b = 3;
+//        int c =4;
+//
+//        int k = b*b - (4*a*c)/(2*a);
+//        System.out.println(k);
+    }
+}
