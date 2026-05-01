01ac227b37b36976dff926baee606b1e18fad888
nikhilesh580 <nikhileshtripathi50@gmail.com>
2026-05-02 01:49:20 +0530

    CWN_09_PS_PR4.java

---
+++ src/CWN_09_PS_PR4.java
@@ -0,0 +1,13 @@
+public class CWN_09_PS_PR4 {
+    public static void main(String[]args){
+         int v = 3;
+         int u = 8;
+         int a = 4;
+         int s = 6;
+
+         int result = (v*v - u*u)/(2*a*s);
+        System.out.println("RESULT: " + result);
+
+    }
+
+}
